46c067eab6bca098a7d923905f24085f7f55229b
kalyanasundaram-venkatesan <kalyanasundaram.venkatesan@freshworks.com>
2023-05-02 18:45:44 +0530

    Adding build script

---
+++ deploy/build_image.groovy
@@ -0,0 +1,40 @@
+DOCKER_TAG = "${env.ECR_IMAGE}:${params.IMAGE_TAG}"
+currentBuild.displayName = "#${currentBuild.id} - ${params.IMAGE_TAG}"
+
+node('spot-webframe-ami-update') {
+  withCredentials([sshUserPrivateKey(credentialsId: env.GITHUB_RUNWAYCI_CREDENTIAL_ID, keyFileVariable: 'identityFile')]) {
+    sh """#!/bin/bash
+      cp ${identityFile} /root/.ssh/id_rsa
+      chmod 400 /root/.ssh/id_rsa
+      ssh-keyscan github.com > /root/.ssh/known_hosts
+    """
+
+    stage('Checkout') {
+      checkout scm: [
+        $class: 'GitSCM',
+        branches: [[name: "*/$BRANCH"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [[
+          credentialsId: env.GITHUB_RUNWAYCI_CREDENTIAL_ID,
+          url: 'git@github.com:freshdesk/conductor.git'
+        ]]
+      ]
+    }
+
+    stage('Build') {
+      dir('deploy/') {
+        sh """#!/bin/bash
+          cp ${identityFile} id_rsa
+        """
+        sh "docker build . -t ${DOCKER_TAG} --force-rm --no-cache --build-arg app_tag=${params.BRANCH}"
+      }
+    }
+
+    stage('Push') {
+      sh "eval \$(aws ecr get-login --registry-ids ${env.AWS_ACCOUNT_ID} --no-include-email --region ${env.AWS_REGION})"
+      sh "docker push ${DOCKER_TAG}"
+    }
+  }
+}
