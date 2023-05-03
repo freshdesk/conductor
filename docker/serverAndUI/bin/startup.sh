4d9bb7cb22300c30755c5db43a3e8e4252dc1c8b
kalyanasundaram-venkatesan <kalyanasundaram.venkatesan@freshworks.com>
2023-05-03 15:23:33 +0530

    changes in startup.sh

--- docker/serverAndUI/bin/startup.sh
+++ docker/serverAndUI/bin/startup.sh
@@ -33,4 +33,4 @@ if [ -z "$CONFIG_PROP" ];
     export config_file=/app/config/$CONFIG_PROP
 fi
 
-nohup java -jar -DCONDUCTOR_CONFIG_FILE=$config_file conductor-server-*-boot.jar 1>&2 > /app/logs/server.log
+java -jar -DCONDUCTOR_CONFIG_FILE=$config_file conductor-server-*-boot.jar
