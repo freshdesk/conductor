51037f4bdc7e51f72917252448e20cae579b0a4f
keerthivaasan-kanagaraj <keerthivaasan.kanagaraj@freshworks.com>
2024-03-22 15:05:02 +0530

    fix

--- server/src/main/java/com/netflix/conductor/Conductor.java
+++ server/src/main/java/com/netflix/conductor/Conductor.java
@@ -28,6 +28,7 @@ import io.micrometer.prometheus.PrometheusMeterRegistry;
 import com.netflix.spectator.api.Registry;
 import com.netflix.spectator.api.Spectator;
 import com.netflix.spectator.micrometer.MicrometerRegistry;
+import io.micrometer.prometheus.PrometheusRenameFilter;
 
 // Prevents from the datasource beans to be loaded, AS they are needed only for specific databases.
 // In case that SQL database is selected this class will be imported back in the appropriate
