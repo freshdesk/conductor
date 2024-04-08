a8a831933a60d86ee3504105844bb05fc7aea9a4
keerthivaasan-kanagaraj <keerthivaasan.kanagaraj@freshworks.com>
2024-04-08 12:04:14 +0530

    custom metrics scraper endpoint

---
+++ server/src/main/java/com/netflix/conductor/CustomPrometheusScrapEndpoint.java
@@ -0,0 +1,27 @@
+package com.netflix.conductor;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import com.netflix.spectator.api.DefaultRegistry;
+import com.netflix.spectator.api.Spectator;
+
+import io.micrometer.core.instrument.Clock;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.CollectorRegistry;
+
+@Endpoint(id="custom_metrics")
+@Component
+public class CustomPrometheusScrapEndpoint {
+    @ReadOperation
+    @Bean
+    public String custom_metrics() {
+        PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT,
+                                                                            CollectorRegistry.defaultRegistry,
+                                                                            Clock.SYSTEM);
+        prometheusRegistry.scrape()
+    }
+}
