0d82a597818a7a8e6d23362eb9a9c7f9cbb5dc37
keerthivaasan-kanagaraj <keerthivaasan.kanagaraj@freshworks.com>
2024-04-08 13:36:46 +0530

    fix

--- server/src/main/java/com/netflix/conductor/CustomPrometheusScrapEndpoint.java
+++ server/src/main/java/com/netflix/conductor/CustomPrometheusScrapEndpoint.java
@@ -22,6 +22,6 @@ public class CustomPrometheusScrapEndpoint {
         PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT,
                                                                             CollectorRegistry.defaultRegistry,
                                                                             Clock.SYSTEM);
-        prometheusRegistry.scrape();
+        return prometheusRegistry.scrape();
     }
 }
