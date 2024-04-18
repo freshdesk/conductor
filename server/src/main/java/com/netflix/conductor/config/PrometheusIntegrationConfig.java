b418e8349fac8fcb4ec9ab7a9e1f485596886486
keerthivaasan-kanagaraj <keerthivaasan.kanagaraj@freshworks.com>
2024-04-18 17:05:50 +0530

    Revert "fix"
    This reverts commit 86c9314360fbe75f89b8aba14682233066db9eac.

--- server/src/main/java/com/netflix/conductor/config/PrometheusIntegrationConfig.java
+++ server/src/main/java/com/netflix/conductor/config/PrometheusIntegrationConfig.java
@@ -31,14 +31,13 @@ public class PrometheusIntegrationConfig
      * To Register PrometheusRegistry
     */
     private static void setupPrometheusRegistry() {
-        // final PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT,
-        //                                                                     CollectorRegistry.defaultRegistry,
-        //                                                                     Clock.SYSTEM);
-        // final MicrometerRegistry metricsRegistry = new MicrometerRegistry(prometheusRegistry);
-        // prometheusRegistry.config().meterFilter(new PrometheusRenameFilter());
-        // Spectator.globalRegistry().add(metricsRegistry);
-        // Metrics.globalRegistry.add(prometheusRegistry);
-        Spectator.globalRegistry().add(Metrics.globalRegistry);
+        final PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT,
+                                                                            CollectorRegistry.defaultRegistry,
+                                                                            Clock.SYSTEM);
+        final MicrometerRegistry metricsRegistry = new MicrometerRegistry(prometheusRegistry);
+        prometheusRegistry.config().meterFilter(new PrometheusRenameFilter());
+        Spectator.globalRegistry().add(metricsRegistry);
+        Metrics.globalRegistry.add(prometheusRegistry);
         log.info("Registered PrometheusRegistry 1");
     }
 
