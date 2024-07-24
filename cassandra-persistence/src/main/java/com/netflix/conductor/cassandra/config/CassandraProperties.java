e1e42d27ad8fe7f488a3584399645422938bf32b
keerthivaasan-kanagaraj <keerthivaasan.kanagaraj@freshworks.com>
2024-07-24 11:05:05 +0530

    fix

--- cassandra-persistence/src/main/java/com/netflix/conductor/cassandra/config/CassandraProperties.java
+++ cassandra-persistence/src/main/java/com/netflix/conductor/cassandra/config/CassandraProperties.java
@@ -48,7 +48,7 @@ public class CassandraProperties {
     private int shardSize = 100;
 
     /** The replication strategy with which to configure the keyspace */
-    private String replicationStrategy = "NetworkTopologyStrategy";
+    private String replicationStrategy = "SimpleStrategy";
 
     /** The key to be used while configuring the replication factor */
     private String replicationFactorKey = "replication_factor";
